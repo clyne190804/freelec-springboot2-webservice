8f1cc92ea0d411e4ea2cbf496a8e78eddc0d5050
clyne <clyne190804@gmail.com>
2020-08-05 04:58:07 +0900

    gitignore등록

--- src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -13,5 +13,9 @@ public class HelloResponseDtoTest {
 
         HelloResponseDto dto = new HelloResponseDto(name, amount);
 
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
     }
+
+
 }
